cd7fa958c52eed459704a2ceb654d0f53142e1ce
wilyJ80 <abjurandam@gmail.com>
2024-09-27 10:23:41 -0300

    tutorial part 2

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    pub_date = models.DateTimeField()
+
+    def was_recently_added(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
